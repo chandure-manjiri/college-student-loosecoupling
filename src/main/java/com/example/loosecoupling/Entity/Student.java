8827add0c551b73a0b3f4d990cf9bd4deb4c9c9c
manjiri-chandure <chanduremanjiri@gmail.com>
2024-02-20 20:27:07 +0530

    initial commit with student entity added

---
+++ src/main/java/com/example/loosecoupling/Entity/Student.java
@@ -0,0 +1,33 @@
+package com.example.loosecoupling.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "students")
+
+public class Student{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "firstname")
+    private String firstName;
+    @Column(name = "lastname")
+    private String lastName;
+    @Column(name = "gender")
+    private String gender;
+    @Column(name = "age")
+    private Integer age;
+    @Column(name = "phonenumber",length = 10)
+    private String phone;
+
+}
