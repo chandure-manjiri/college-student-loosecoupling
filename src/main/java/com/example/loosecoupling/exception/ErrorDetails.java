4c3a121052f352f29e87e61b567f3625d17db9d1
manjiri-chandure <chanduremanjiri@gmail.com>
2024-03-04 10:57:05 +0530

    respoceStatus HTTPStatus.NotFound removed from ResourceNotFoundException

--- src/main/java/com/example/loosecoupling/exception/ErrorDetails.java
+++ src/main/java/com/example/loosecoupling/exception/ErrorDetails.java
@@ -5,7 +5,6 @@ import java.util.Date;
 public class ErrorDetails {
     private Date timestamp;
     private String message;
-
     private String details;
 
     public ErrorDetails(Date timestamp, String message, String details){
